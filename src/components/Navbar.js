0dc364765f54998669ea8a89eed97b2a0937792e
sping-coding <parkin153@naver.com>
2022-08-19 20:33:10 +0900

    9

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Navbar.css";
 import { useNavigate } from "react-router-dom";
-import { UseState } from "react";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -17,20 +16,49 @@ const Navbar = () => {
   const goToBoard = () => {
     navigate("/board");
   };
-  const { log, SetLog } = useState(false);
 
-  return (
-    <div className="MainNavBar">
-      <div className="MainLogo">Caerulea</div>
-      <div className="MainMenu">
-        <button onClick={goToProduct}>쇼핑몰</button>
-        <span>I</span>
-        <button onClick={goToBoard}>커뮤니티</button>
-        <span>I</span>
-        <button onClick={goToLogin}>로그인</button>
+  const userid = window.sessionStorage.getItem("id");
+
+  const handleLogout = () => {
+    console.log("handleLogout");
+    window.sessionStorage.clear();
+    console.log(
+      "handleLogout:window.sessionStorage(login_id) =>",
+      window.sessionStorage.getItem("id")
+    );
+    navigate("/"); // 로그인 페이지로 이동
+  };
+
+  if (userid === null) {
+    return (
+      <div className="MainNavBar">
+        <div className="MainLogo"> Caerulea</div>
+        <div className="MainMenu">
+          <button onClick={goToProduct}>쇼핑몰</button>
+          <span>I</span>
+          <button onClick={goToBoard}>커뮤니티</button>
+          <span>I</span>
+          <button onClick={goToLogin}>로그인</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else if (userid !== null) {
+    return (
+      <div className="MainNavBar">
+        <div className="MainLogo">
+          {/* Caerulea */}
+          {userid} 님 환영합니다
+        </div>
+        <div className="MainMenu">
+          <button onClick={goToProduct}>쇼핑몰</button>
+          <span>I</span>
+          <button onClick={goToBoard}>커뮤니티</button>
+          <span>I</span>
+          <button onClick={handleLogout}>로그아웃</button>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Navbar;
