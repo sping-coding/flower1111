8621cd81a0b8ca1a0bede46546d94595dd93b846
sping-coding <parkin153@naver.com>
2022-08-27 14:04:50 +0900

    good

--- src/Mypage/MypageForm.js
+++ src/Mypage/MypageForm.js
@@ -6,16 +6,21 @@ const MypageForm = () => {
   const usernick = window.sessionStorage.getItem("nick");
   const useraddr = window.sessionStorage.getItem("addr");
   const usertel = window.sessionStorage.getItem("tel");
+  const userid = window.sessionStorage.getItem("id");
 
   console.log(usernick, useraddr, usertel);
 
   return (
     <div className="MyPages">
       <div className="MyPagesWel">
-        <p>MY PAGE</p>
+        <p>마이페이지</p>
         <h2>{usernick} 님 반갑습니다</h2>
       </div>
       <div className="MyPagesDet">
+        <div className="MyPagesDetID">
+          <p>아이디 | </p>
+          <p>{userid}</p>
+        </div>
         <div className="MyPagesDetAddr">
           <p>주소 | </p>
           <p>{useraddr}</p>
--- src/Payment/PaymentShot.js
+++ src/Payment/PaymentShot.js
@@ -1,36 +1,21 @@
 import React from "react";
 
-const PaymentShot = ({paymentlist}) => {
-    console.log("PaymentShot(paymentList) =>", paymentlist.paymentList)
-    return(
-        <div>
-        {paymentlist.paymentList?.map((pay) => (
-            <div>
-                <table>
-                    <tbody>
-                        <tr>
-                            <td>
-                                주문번호 : {pay.num}
-                            </td>
-                            <td>
-                                이미지 : <img src={pay.image_url} width="50px" height="50px"/>
-                            </td>
-                            <td>
-                                상품이름 : {pay.irum}
-                            </td>
-                            <td>
-                                갯수 : {pay.quantity}
-                            </td>
-                            <td>
-                                가격 : {pay.price}
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        ))}
-     
+const PaymentShot = ({ paymentlist }) => {
+  console.log("PaymentShot(paymentList) =>", paymentlist.paymentList);
+  return (
+    <div>
+      {paymentlist.paymentList?.map((pay) => (
+        <div className="payList">
+          <div className="payLists">주문번호 : {pay.num}</div>
+          <div className="payLists payListsimg">
+            <img src={pay.image_url} width="50px" height="50px" />
+          </div>
+          <div className="payLists">상품이름 : {pay.irum}</div>
+          <div className="payLists">갯수 : {pay.quantity}</div>
+          <div className="payLists">가격 : {pay.price}</div>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 export default PaymentShot;
