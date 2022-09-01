ecb4dd7e22dc5d21cc4b395e8b352194574651f0
sping-coding <parkin153@naver.com>
2022-09-01 12:12:54 +0900

    hihi

--- src/Mypage/MypageForm.js
+++ src/Mypage/MypageForm.js
@@ -21,87 +21,60 @@ const MypageForm = () => {
     <div>
       <div className="MyPages">
         <div className="MyPagesWel">
-          <p>마이페이지</p>
+          <p className="MyPagesWelF">MY&nbsp;PAGE</p>
           <p>{usernick} 님 반갑습니다</p>
         </div>
-        <div className="MyPagesDet">
-          <div className="MyPagesDetID">
-            <p>아이디 &nbsp;:</p>
-            <p>{userid}</p>
+        <div className="MypageDates">
+          <div className="MyPagesDet">
+            <div className="MyPagesDetF">
+              <div className="MyPagesDetID">
+                <p>아이디 &nbsp;|</p>
+                <p>{userid}</p>
+              </div>
+              <div className="MyPagesDetTel">
+                <p>전화번호&nbsp; |</p>
+                <p>{usertel}</p>
+              </div>
+            </div>
+            <div className="MyPagesDetAddr">
+              <p>주소지&nbsp; |</p>
+              <p>{useraddr}</p>
+            </div>
           </div>
-          <div className="MyPagesDetAddr">
-            <p>주소 &nbsp;: </p>
-            <p>{useraddr}</p>
-          </div>
-          <div className="MyPagesDetTel">
-            <p>전화번호&nbsp; :</p>
-            <p>{usertel}</p>
-          </div>
-        </div>
 
-        <h3>주문조회</h3>
-        <div className="MypageDelivers">
-          <div className="MypageD MypageDBlue">
-            <p className="MypageDDB">발송준비중</p>
-            <BsBoxSeam size="37" />
-            <p className="MypageDD">주문하신 상품을 준비하고 있습니다.</p>
+          <h3>배송조회</h3>
+          <div className="MypageDelivers">
+            <div className="MypageD MypageDBlue">
+              <p className="MypageDDB">발송준비중</p>
+              <BsBoxSeam size="37" />
+              <p className="MypageDD">주문하신 상품을 준비하고 있습니다.</p>
+            </div>
+            <div className="MypageArrow">
+              <BsFillArrowRightCircleFill size="37" />
+            </div>
+            <div className="MypageD">
+              <p>배송중</p>
+              <BsTruck size="37" />
+              <p className="MypageDD">택배사로 상품이 전달되었습니다.</p>
+            </div>
+            <div className="MypageArrow">
+              <BsFillArrowRightCircleFill size="37" />
+            </div>
+            <div className="MypageD">
+              <p>배송완료</p>
+              <FaBoxOpen size="37" />
+              <p className="MypageDD">고객님께 상품이 배송되었습니다.</p>
+            </div>
           </div>
-          <div className="MypageArrow">
-            <BsFillArrowRightCircleFill size="37" />
-          </div>
-          <div className="MypageD">
-            <p>배송중</p>
-            <BsTruck size="37" />
-            <p className="MypageDD">택배사로 상품이 전달되었습니다.</p>
-          </div>
-          <div className="MypageArrow">
-            <BsFillArrowRightCircleFill size="37" />
-          </div>
-          <div className="MypageD">
-            <p>배송완료</p>
-            <FaBoxOpen size="37" />
-            <p className="MypageDD">고객님께 상품이 배송되었습니다.</p>
+
+          <div className="MyPageLook">
+            <div className="Mypageitemsss">
+              <PaymentList />
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="MyPageLook">
-        <div className="Mypageitemsss">
-          <PaymentList />
-        </div>
-      </div>
-
-      {/* <div className="MypageInform">
-        <div className="MypageInformCall">
-          <h5>- 대표전화 -</h5>
-          <div className="MypageInformCallSS">
-            <p className="MypageInformCallSSF">010-4622-2173</p>
-            <p>평일09:00 ~ 19:00 &nbsp;|&nbsp; 토,일,공휴일 휴무</p>
-          </div>
-        </div>
-        <div className="MypageInformSNS">
-          <h5>- 크룰리아 SNS -</h5>
-          <div className="MypageInformSNSCircle">
-            <div>
-              <img src={Blog} />
-            </div>
-            <div>
-              <img src={Face} />
-            </div>
-          </div>
-        </div>
-        <div className="MypageInformTalk">
-          <h5>- 실시간 상담 -</h5>
-          <div className="MypageInformTalkDD">
-            <p>실시간 상담 가능합니다</p>
-            <div className="MypageInformTalkDDDK">
-              <a href="https://pf.kakao.com/_fRXlxj" target="_blank">
-                <div className="MypageInformTalkDDDKBTN">카카오톡 상담</div>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div> */}
       <div className="MypageBackground">
         <img className="MypageBackgroundsss" src={MyBack} />
         <img className="MypageBlogSNS" src={Blog} />
